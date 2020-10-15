37db5271f268577fc22b2302d052853a519afd43
Linghao_Chan <Linghao_Chan@163.com>
2020-10-15 21:53:07 +0800

    make title to vec

--- get_data.py
+++ get_data.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 import os
 import pandas as pd
 import re
+import sentence2vec
+from sklearn.manifold import TSNE
 
 class Get_Data():
     
@@ -13,12 +15,12 @@ class Get_Data():
         """
         Read Movie DataSets
         """
-        # read file
+        ''' read file'''
         new_movies = pd.DataFrame(columns=['MovieID', 'Title', 'Genres', 'Year'])
         movies_title = ['MovieID', 'Title_Year', 'Genres']
         movies = pd.read_csv(path, sep='::', header=None, names=movies_title, engine = 'python')
 
-        # map Genres into number 0, 1, 2...
+        ''' map Genres into number 0, 1, 2...'''
         movie_genres_map = {
             "Action" : 0,
             "Adventure" : 1,
@@ -41,9 +43,11 @@ class Get_Data():
         }
 
         pattern = re.compile(r'\((\d+)\)$')
-        # seperate title and year
+        ''' seperate title and year'''
+        Title_list = []
         for index, row in movies.iterrows():
             Title = re.sub(pattern, "", row['Title_Year'])
+            Title_list.append(Title)
             Year = row['Title_Year'].replace(Title, '')
             Year = Year.strip('(')
             Year = Year.strip(')')
@@ -58,8 +62,19 @@ class Get_Data():
                 index=[1]
             ) 
             new_movies=new_movies.append(new,ignore_index=True)
+
+        ''' convert title into array by TfidfVectorizer and t-SNE'''
+        if not os.path.exists("title_array.npy"):
+            s2v = sentence2vec.SentenceToVector()
+            title_embedded = s2v.model(Title_list)
+        
+        ''' dataframe -> dictionary'''
         movie_list = new_movies.to_dict('records')
+        # print(title_embedded.shape)
+        # s2v.plotarr(title_embedded)
         # make Genres into list-vector
+        title_array = np.load("title_array.npy")
+        i = 0
         for movie in movie_list:
             Genres = movie['Genres']
             Genres_list_str = Genres.split('|')
@@ -67,6 +82,8 @@ class Get_Data():
             for Genres_str in Genres_list_str:
                 Genres_list_int[movie_genres_map[Genres_str]] = 1
             movie['Genres'] = Genres_list_int
+            movie['Title'] = title_array[i, :]
+            i += 1
         # print(movie_list)
         self.movies_data = movie_list
         return new_movies
@@ -75,16 +92,16 @@ class Get_Data():
         """
         Read User Datasets
         """
-        # read file
+        ''' read file'''
         user_title = ['UserID', 'Gender', 'Age', 'Occupation', 'Zip-code']
         users = pd.read_csv(path, sep='::', header=None, names=user_title, engine = 'python')
-        # map Sex to 0, 1
+        ''' map Sex to 0, 1'''
         gender_map = {'F':0, 'M':1}
         users['Gender'] = users['Gender'].map(gender_map)
-        # map Age to number 0, 1, 2...
+        ''' map Age to number 0, 1, 2...'''
         age_map = {val:ii for ii,val in enumerate(set(users['Age']))}
         users['Age'] = users['Age'].map(age_map)
-        # delete Zip-code
+        ''' delete Zip-code'''
         users = users.drop('Zip-code', axis = 1)
         users_list = users.to_dict('records')
             
@@ -95,7 +112,7 @@ class Get_Data():
 
 if __name__ == "__main__":
     movie = Get_Data()
-    # movie.get_movie_message()
-    # print(movie.movies_data)
-    # movie.get_user_message()
-    # print(movie.users_data)
+    movie.get_movie_message()
+    print(movie.movies_data)
+    movie.get_user_message()
+    print(movie.users_data)
---
+++ sentence2vec.py
@@ -0,0 +1,22 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+import numpy as np
+
+class SentenceToVector():
+    def model(self, text_list):
+        cv=TfidfVectorizer(binary=False,decode_error='ignore',stop_words='english')
+        vec=cv.fit_transform(text_list)
+        arr=vec.toarray()
+        print("preparing for tsne...")
+        arr_embedded = TSNE(n_components=3).fit_transform(arr)
+        print("finished for tsne...")
+        np.save("title_array.npy",arr_embedded) 
+        print("tsne saved")
+        # plotarr(arr)
+        return arr
+    def plotarr(self, arr):
+        x = arr[: , 0]
+        y = arr[: , 1]
+        plt.scatter(x, y)
+        plt.show()
