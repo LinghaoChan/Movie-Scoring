07210099dc45d6968f3e25b14a36d3d45c06f4dd
LinghaoChan <Linghao_Chan@163.com>
2020-10-26 22:19:21 +0800

    Update training_model.py

--- training_model.py
+++ training_model.py
@@ -1,7 +1,17 @@
 import numpy as np
 import torch
+from torch.utils.data import DataLoader,Dataset
 from matplotlib import pyplot as plt
 import os
 import pandas as pd
 import re
+import get_data
 
+class MovieScoringDataSets(Dataset):
+    def __init__(self, ):
+
+
+    def __len__(self):
+
+        
+    def __getitem__(self):
